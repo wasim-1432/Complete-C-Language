8025c0475a44fd78dadab34c0d43d19cc5a8edb5
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-08 20:56:56 +0530

    Write a C program to calculate area of rectangle.

---
+++ wasim9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int length,breadth;
+    printf("Enter the length and breadth of the rectangle\n");
+    scanf("%d%d",&length,&breadth);
+    printf("Area of Rectangle=%d",length*breadth);
+    printf("\n");
+    return 0;
+}
