6c1e980acd1e04f17a0fe5208eba4e7ca2f72965
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-08 21:14:18 +0530

    Write a C program to calculate volume of cuboid.

---
+++ wasim14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int length,breadth,height;
+    printf("Enter the length,breadth and height\n");
+    scanf("%d%d%d",&length,&breadth,&height);
+    printf("Volume of cuboid =%d",length*breadth*height);
+    printf("\n");
+    return 0;
+}
