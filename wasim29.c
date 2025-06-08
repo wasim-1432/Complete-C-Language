989393736741891e40b024dbd2b5d17d030e3e60
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-08 22:01:17 +0530

    Write a C program to print size of int,float,char and double type variables.

---
+++ wasim29.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of int type variables ::%d\n",sizeof(int));
+    printf("Size of int type variables ::%d\n",sizeof(float));
+    printf("Size of int type variables ::%d\n",sizeof(char));
+    printf("Size of int type variables ::%d\n",sizeof(double));
+    printf("\n");
+    return 0;
+}
