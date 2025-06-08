1e06a60fa22d8256ba80ab799a96d7536f4d1c12
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-08 22:10:42 +0530

    Write a C program to input a number from the user and also input a digit.Append a digit in a number and print the resulting number.

---
+++ wasim31.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,d;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    printf("Enter any digit\n");
+    scanf("%d",&d);
+    printf("Required answer is=%d",n*10+d);
+    printf("\n");
+    return 0;
+}
