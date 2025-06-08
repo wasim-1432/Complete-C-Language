dcd1e42616ce8437207f24f9a7c433c4583e195a
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-08 21:31:31 +0530

    Write a C program to print witout last digit of a given numbers.

---
+++ wasim20.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any nuber\n");
+    scanf("%d",&a);
+    printf("Without Last digit of a given number=%d",a/10);
+    printf("\n");
+    return 0;
+}
