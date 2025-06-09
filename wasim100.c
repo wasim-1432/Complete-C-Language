d4743bcc0d4168f085716f87128a7c323e0e91b9
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-09 18:03:55 +0530

    Write a C program to print following pattern :: ABCDE  BCDE   CDE    DE     E

---
+++ wasim100.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        char k=64+i;
+        for(j=1;j<=5;j++)
+        {
+            if(j>=i)
+            {
+                printf("%c",k++);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
